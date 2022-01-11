fdcdc6dd72696512d8d08f3fa7306dd1ab5375d6
Ziad Khaled <ziadkhaled1419@gmail.com>
2022-01-11 17:29:35 +0100

    Basic project skeleton

--- app/server/routes/users.js
+++ app/server/routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/login', function(req, res) {
+
+});
+
+router.post('/', function(req, res) {
+
+});
+
+
+// //export this router to use in our app.js
 module.exports = router;
