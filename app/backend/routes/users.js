00c9b77fc3e9c70008236662940c66d24e62a989
Ziad Khaled <ziadkhaled1419@gmail.com>
2022-01-11 18:07:44 +0100

    Reorganize repo

--- app/backend/routes/users.js
+++ app/backend/routes/users.js
@@ -6,14 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/login', function(req, res) {
-
-});
-
-router.post('/', function(req, res) {
-
-});
-
-
-// //export this router to use in our app.js
 module.exports = router;
