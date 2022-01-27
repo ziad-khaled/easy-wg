fabac086cdd45ae2f7af76eca940d7a6365defad
Hasib Iqbal <fdai6662@gitlab.cs.hs-fulda.de>
2022-01-27 19:57:56 +0100

    Logged In UI design is done

--- easywg/App.js
+++ easywg/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 //import Login from './components/Login/Login';
 import Registration from './components/Registration/Registration';
+import LoggedIn from './components/LoggedIn/LoggedIn';
 
 export default class App extends React.Component {
   render(){
     return(
       //<Login></Login>
-      <Registration></Registration>
+      //<Registration></Registration>
+      <LoggedIn/>
     );
   }
 }
