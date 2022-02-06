fa06d896443b72328039cce30aad2f91c90b073f
Anwer AL_Dhify <anwer-ahmed.al-dhify@informatik.hs-fulda.de>
2022-02-06 13:46:10 +0100

    Update associations

--- app/backend/models/associations.js
+++ app/backend/models/associations.js
@@ -1,9 +1,36 @@
 const sequelize = require('./db_connection')
 const User = require('./User')
 const WG = require('./WG')
+const Announcement = require('./Announcement')
+const Purchase = require('./Purchase')
+const Task = require('./Task')
 
 User.hasOne(WG);
 WG.belongsTo(User, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE', 
+    as: 'admin', 
+    foreignKey: 'adminID'
+});
+
+User.hasOne(Announcement);
+Announcement.belongsTo(User, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE', 
+    as: 'admin', 
+    foreignKey: 'adminID'
+});
+
+User.hasOne(Purchase);
+Purchase.belongsTo(User, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE', 
+    as: 'admin', 
+    foreignKey: 'adminID'
+});
+
+User.hasOne(Task);
+Task.belongsTo(User, {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE', 
     as: 'admin', 
